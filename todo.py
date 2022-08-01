36e13e4c06ac06bbd46c799e08dbeef7cd42330b
Ioannis E. Kommas <54149422+johnkommas@users.noreply.github.com>
2022-08-02 00:18:12 +0300

    @Ioannis E. Kommas

--- todo.py
+++ todo.py
@@ -1,5 +1,23 @@
 #  Copyright (c) Ioannis E. Kommas 2022. All Rights Reserved
 
+# TODO Initialize SQL and VPN Data
+"""
+Slack HOME PAGE BUTTON (OK)
+Slack MODAL POP UP     (OK)
+SLack Action           (OK)
+"""
+
+# TODO CHANGE CREDENTAILS INPUT
+"""
+SQL CONNECT --> Να τραβάει τις πληροφορίες σύνδεσης από την Βάση Δεδομένων αντί το αρχείο           (ΟΚ)
+crendentials --> να βρω τρόπο τα credentials να κρυπτογραφούνται κατά την εγγραφή τους στην βάση.   (ΟΚ)
+"""
+
+# TODO SQL and VPN UPDATE
+"""
+Να δημιουργήσω στο SLACK ΗΟΜΕ, BUTTON που να κάνει UPDATE τα CREDENTILAS   (PENDING)
+"""
+
 # TODO Να φτιάξω τελικό αποτέλεσμα με τιμές από τιμοκατάλογο
 """
 Ερώτημα στην DB                                                (OK)
